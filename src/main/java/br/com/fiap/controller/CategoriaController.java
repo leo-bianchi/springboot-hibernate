01a7f32d3c4ecb845cbee467231bb86f9a4df343
Leonardo Bianchi <leo.c.bianchi@hotmail.com>
2019-05-30 23:43:51 -0300

    Add comments

--- src/main/java/br/com/fiap/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/controller/CategoriaController.java
@@ -18,47 +18,65 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.fiap.model.CategoriaModel;
 import br.com.fiap.repository.CategoriaRepository;
 
+// Devemos usar a notação para indicar que a classe é Controller
 @Controller
+// Definindo que tudo estará dentro do caminho /categoria
 @RequestMapping("/categoria")
 public class CategoriaController {
 	
+	// Definimos 'categoria/' como uma constante para não ter que repetir inúmeras vezes no código
 	private static final String CATEGORIA_PATH = "categoria/";
 
+	// Instanciamos a CategoriaRepository para acessarmos os métodos do Hibernate
 	@Autowired
 	public CategoriaRepository categoriaRepository;
 	
-	@GetMapping("/relatorio")
+	// Listagem
+	/* Passamos uma Model como parâmetro e adicionamos os valores recebidos na variável
+	"categorias" (utilizaremos esse nome na .jsp. 
+	O retorno será o nome do arquivo .jsp para onde desejamos que o usuário seja encaminhado
+	Utilizamos o método GET para buscas -> GetMapping */
+	@GetMapping("/relatorio") // caminho na url (/categoria/relatorio)
 	public String getProductsByCategories(Model model) {
+		// Adicionando os dados do banco a variável "categorias"
 		model.addAttribute("categorias", categoriaRepository.findAll());
+		// Enviar para o arquivo categoria/categorias-relatorio.jsp
 		return CATEGORIA_PATH + "categorias-relatorio";
 	}
 	
 	// Busca
+	// RequestParam porque receberemos um parâmetro, no caso isNew
 	@GetMapping()
 	public String get(@RequestParam(required=false) boolean isNew, Model model) {	
-		
+		// Se isNew for true, irá criar uma nova categoriaModel
 		if(isNew) {
 			model.addAttribute("categoriaModel", new CategoriaModel());	
 		} else {
+		// Se for falso fara a busca de categorias
 			model.addAttribute("categorias", categoriaRepository.findAll());	
 		}
-		
+		// If ternário
+		// Se isNew for true, leva para categoria-novo, senão, para categorias
 		return CATEGORIA_PATH + (isNew ? "categoria-novo" : "categorias");
 	}
 	
+	// Mapeia um id para url
 	@GetMapping(value = "/{id}")
 	public String getById(@PathVariable("id") long id, @RequestParam(required=false) boolean isEdit, Model model) {
-
+		// No Hibernate quando utilizamos findById(id) deve ter um .get() no final para funcionar.
 		model.addAttribute("categoriaModel", categoriaRepository.findById(id).get());
 		
+		// Se isEdit for true, encaminha para categoria-editar, senão para categoria-detalhe
 		return CATEGORIA_PATH + (isEdit ? "categoria-editar": "categoria-detalhe");
 	}
 	
 	// Cadastro
+	// Para cadastro utilizamos método POST
 	@PostMapping()
 	public String save(@Valid CategoriaModel categoriaModel, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-		
+		// Se não tiver erros
 		if (!bindingResult.hasErrors()) {
+			// Envia a mensagem de sucesso e redireciona para a página da categoria criada
 			redirectAttributes.addFlashAttribute("mensagemCategoria", "Categoria cadastrada com sucesso");
 			categoriaRepository.save(categoriaModel);
 			return "redirect:/categoria";
@@ -68,6 +86,7 @@ public class CategoriaController {
 	}
 	
 	// Edicao
+	// Edição utiliza método PUT
 	@PutMapping()
 	public String edit(@Valid CategoriaModel categoriaModel, 
 			BindingResult bindingResult, RedirectAttributes redirectAttributes) {
@@ -83,11 +102,12 @@ public class CategoriaController {
 	}
 	
 	
-	// Deleção	
+	// Deleção
 	@DeleteMapping("/{id}")
 	public String delete(@PathVariable("id") long id, RedirectAttributes redirectAttributes) {
-		
+		// Recebe o ID da categoria a ser deletada
 		categoriaRepository.deleteById(id);
+		// Envia a mensagem
 		redirectAttributes.addFlashAttribute("mensagemCategoria", "Categoria excluída com sucesso");
 		return "redirect:/categoria";
 	}
